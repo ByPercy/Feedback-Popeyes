080df8e137204c7c34c2fe562fd420d9a85fe4b3
Percy Maza <josephmazabautista@gmail.com>
2026-01-12 23:24:30 -0500

    Form-Popeyes-V4.5

--- server.js
+++ server.js
@@ -21,12 +21,14 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-//Transporte de correo
+//Transporte de correo a Brave
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 });
 
